5728045d796ecd41738c871a2e08d000322881dd
jrourkeEHS <144248718+jrourkeEHS@users.noreply.github.com>
2025-06-16 09:58:15 +0100

    Core infra config

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
 
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0d1ddd83282187d18" # Ubuntu 22.04 in eu-central-1
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
